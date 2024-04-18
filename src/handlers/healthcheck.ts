9bb0f7eadfbf9370794238538fc16818a1f42e2f
0xAckerMan <joelkores1@gmail.com>
2024-04-18 12:56:47 +0300

    feat: Healtchecker for the api

---
+++ src/handlers/healthcheck.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from 'express'
+export const Healthcheck = (req: Request, res: Response) => {
+    const status = {
+        "status": "active",
+        "environment": "dev",
+        "version": "1.0.0"
+    }
+
+    res.json({"Healthcheck": status})
+}
