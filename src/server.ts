91f30bdb2a417a917d3077220218cebfdd51025e
0xAckerMan <joelkores1@gmail.com>
2024-04-19 00:02:27 +0300

    feat: use express json to decode json req and morgan logger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import {router} from './routes'
+import morgan from 'morgan'
 
 const app = express()
 
+app.use(express.json())
+app.use(morgan('dev'))
 app.use("/api/v1", router)
 
 export {app}
